3f60a2f5dba794d61f904a9365b36fda2e04053b
manhxuanpham <manhhoathuan@gmail.com>
2024-10-06 20:52:36 +0700

    first commit

---
+++ Data/NZWalksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZwalks.Models.Domain;
+
+namespace NZwalks.Data
+{
+    public class NZWalksDbContext:DbContext
+    {
+        public NZWalksDbContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+        {
+            
+        }
+        public DbSet<Difficulty> Difficulty { get; set; }
+        public DbSet<Region> Region { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
